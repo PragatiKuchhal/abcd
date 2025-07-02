f658c13d2a224975648e49f710ad92353ce983f9
PragatiKuchhal <pragati.kuchhal@gmail.com>
2025-07-02 12:36:53 +0530

    adding new java file

---
+++ DataTypes/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        float floatValue1 = 1.2f;
+        float floaValue2 = 1.3f;
+        float floatResult = floatValue1 + floaValue2;
+
+        System.out.println("Float value = " + floatResult);
+
+        double doubleValue1 = 2.67;
+        double doubleValue2 = 7.89;
+        double doubleResult = doubleValue1 + doubleValue2;
+
+        System.out.println("Double value = " + doubleResult);
+    }
+}
