49a444a7ac5c3a6d60694b84bb95f243d0d71a01
PragatiKuchhal <pragati.kuchhal@gmail.com>
2025-07-03 12:13:39 +0530

    adding new java file

---
+++ JavaClass3/Operators.java
@@ -0,0 +1,16 @@
+
+
+public class Operators {
+   public static void main(String[] args) {
+	int a = 20;
+	int b = 10;
+	int result = a + b;
+	int result1 = a - b;
+	int result2 = a * b;
+	int result3 = a / b;
+	System.out.println("The addition of" + " " + a + " and" +" "+  b + " is: " + result);
+	System.out.println("The subtraction of" + " " + a + " and" +" "+  b + " is: " + result1);
+	System.out.println("The multiplication of" + " " + a + " and" +" "+  b + " is: " + result2);
+	System.out.println("The division of" + " " + a + " and" +" "+  b + " is: " + result3);
+ }
+}
